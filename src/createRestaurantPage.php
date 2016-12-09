2299ef5a95bf3c69921c6a0ce27f253ccb1757b9
Ana Rita Torres <anaritatorres96@outlook.pt>
2016-12-09 10:10:10 +0000

    css styling

--- src/createRestaurantPage.php
+++ src/createRestaurantPage.php
@@ -2,3 +2,13 @@
     include('resources/templates/header.php');
     include('resources/templates/restaurant_form.php');
     include('resources/templates/footer.php');
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rock+Salt">
+</head>
+<body>
+</body>
+</html>
--- src/mainPage.php
+++ src/mainPage.php
@@ -2,11 +2,20 @@
    include('resources/templates/header.php');
 ?>
 
-<div class="main">
-    <p>Welcome <?php session_start(); echo $_SESSION['username']; ?>!</p>
-    <a href="editUserPage.php">Edit User Information</a><br>
-    <a href="restaurantsPage.php">Restaurants</a><br>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rock+Salt">
+ </head>
+ <body>
+     <div class="main">
+        <p>Welcome <?php session_start(); echo $_SESSION['username']; ?>!</p>
+        <a href="editUserPage.php">Edit User Information</a><br>
+        <a href="restaurantsPage.php">Restaurants</a><br>
+    </div>
+</body>
+</html>
 
 <?php
 	include('resources/templates/footer.php');
